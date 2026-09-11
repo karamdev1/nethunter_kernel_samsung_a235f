a2f60fac6668a03bcb371fd4c79dc9ac37e70fee
karamdev1 <karamgaggi100@gmail.com>
2026-09-11 23:58:31 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WIP
+## WORK IN PROGRESS
---
+++ build.sh
@@ -0,0 +1,227 @@
+#!/bin/bash
+
+if ! command -v figlet >/dev/null 2>&1; then
+    sudo apt install -y figlet
+fi
+
+if [ ! -f config ]; then
+    echo "\e[1;31m[-] config file not found\e[0m"
+    exit 1
+fi
+
+source config
+
+TYPE=$(eval echo $Type)
+BIN_PATH=$(eval echo $BIN_PATH)
+export PATH="$BIN_PATH:$PATH"
+export ARCH=$(eval echo $ARCH)
+export REAL_CC=$(eval echo $CC)
+export CLANG_TRIPLE=$(eval echo $CLANG_TRIPLE)
+export CROSS_COMPILE=$(eval echo $CROSS_COMPILE)
+OUT_DIR=$(eval echo $OUT_DIR)
+KDIR=$(pwd)
+export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+KERNEL_ENV="DTC_EXT=$KDIR/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+
+BOLDGREEN="\e[1;32m"
+BOLDRED="\e[1;31m"
+BOLDBLUE="\e[1;96m"
+BOLDYELLOW="\e[1;33m"
+ENDCOLOR="\e[0m"
+
+function show_gui() {
+	if [ -f "$KDIR/$OUT_DIR/.config" ]; then
+		CONFIG_STATUS="${BOLDGREEN}Config present$ENDCOLOR"
+	else
+		CONFIG_STATUS="${BOLDRED}No .config found$ENDCOLOR"
+	fi
+	clear
+	echo -e "\e[1;93m"
+	figlet Kernel Builder
+	echo -e "\e[0m"
+	echo -e "${BOLDGREEN}By: ${BOLDYELLOW}Karam (karamdev1)$ENDCOLOR"
+	echo -e "${BOLDGREEN}Kernel Config: ${CONFIG_STATUS}$ENDCOLOR"
+	echo
+	echo -e "$BOLDGREEN|-----------------------------------------------------|$ENDCOLOR"
+	echo -e "$BOLDGREEN| Actions:                                            |$ENDCOLOR"
+	echo -e "$BOLDGREEN|-------------------${ENDCOLOR}Kernel$BOLDGREEN----------------------------|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}1$ENDCOLOR] Compile Kernel                           $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}2$ENDCOLOR] Compile Module $BOLDBLUE(Prepare before Compile!) $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}3$ENDCOLOR] Prepare Module                           $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}4$ENDCOLOR] Copy Modules   $BOLDBLUE(Compile before Copy!)    $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|-----------------------------------------------------|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}5$ENDCOLOR] Clean Kernel    $BOLDBLUE(Clean & Mrproper)       $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}6$ENDCOLOR] Apply Defconfig $BOLDBLUE(Selection in Config)    $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|-----------------------------------------------------|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}7$ENDCOLOR] Edit Config $BOLDBLUE(MENUCONFIG) $BOLDYELLOW(GOOD)          $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}8$ENDCOLOR] Edit Config $BOLDBLUE(NCONFIG) $BOLDGREEN(BEST)             $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}9$ENDCOLOR] Save .config as new defconfig            $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|-------------------${ENDCOLOR}Script$BOLDGREEN----------------------------|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDRED}E$ENDCOLOR] Exit Builder                             $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}G$ENDCOLOR] Open the creator's github page           $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|-------------------${ENDCOLOR}End$BOLDGREEN-------------------------------|$ENDCOLOR"
+	echo
+}
+
+while true; do
+	show_gui
+	echo -ne "${BOLDGREEN}Enter the action: $ENDCOLOR"
+	read action
+
+	case $action in
+		1)
+			echo -e "$BOLDGREEN[+] Building$ENDCOLOR"
+			if [ ! -f "$KDIR/$OUT_DIR/.config" ]; then
+				echo -e "$BOLDRED[-] No .config found$ENDCOLOR"
+			else
+				echo -e "$BOLDGREEN[+] .config found$ENDCOLOR"
+				make -s -C "$KDIR" O="$OUT_DIR" $KERNEL_ENV CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j"$(nproc)"
+				ret=$?
+				if [ $ret -eq 0 ]; then
+					echo -e "$BOLDGREEN[+] Kernel Building Succeed$ENDCOLOR"
+					make -C "$KDIR" O="$OUT_DIR" $KERNEL_ENV INSTALL_MOD_PATH="../out_modules" CONFIG_SECTION_MISMATCH_WARN_ONLY=y modules_install -j"$(nproc)"
+					ret=$?
+					if [ $ret -eq 0 ]; then
+						echo -e "$BOLDGREEN[+] You can find the modules in '$OUT_DIR/modules'$ENDCOLOR"
+					else
+						echo -e "$BOLDGREEN[-] Modules saving to '$OUT_DIR/modules' Failed$ENDCOLOR"
+					fi
+				else
+					echo -e "$BOLDRED[-] Kernel Building Failed (exit code: $ret)$ENDCOLOR"
+				fi
+			fi
+			;;
+		2)
+			echo -e "$BOLDGREEN[+] Building Modules$ENDCOLOR"
+			if [ ! -f "$KDIR/$OUT_DIR/.config" ]; then
+				echo -e "$BOLDRED[-] No .config found$ENDCOLOR"
+			else
+				echo -e "$BOLDGREEN[+] .config found$ENDCOLOR"
+				make -s -C "$KDIR" O="$OUT_DIR" $KERNEL_ENV CONFIG_SECTION_MISMATCH_WARN_ONLY=y modules -j"$(nproc)"
+				ret=$?
+				if [ $ret -eq 0 ]; then
+					echo -e "$BOLDGREEN[+] Modules Building Succeed$ENDCOLOR"
+					echo -e "$BOLDGREEN[+] You can find the modules in out_modules$ENDCOLOR"
+				else
+					echo -e "$BOLDRED[-] Modules Building Failed (exit code: $ret)$ENDCOLOR"
+				fi
+			fi
+			;;
+		3)
+			echo -e "$BOLDGREEN[+] Preparing Modules$ENDCOLOR"
+			if [ ! -f "$KDIR/$OUT_DIR/.config" ]; then
+				echo -e "$BOLDRED[-] No .config found$ENDCOLOR"
+			else
+				echo -e "$BOLDGREEN[+] .config found$ENDCOLOR"
+				make -s -C "$KDIR" O="$OUT_DIR" $KERNEL_ENV CONFIG_SECTION_MISMATCH_WARN_ONLY=y modules_prepare -j"$(nproc)"
+				ret=$?
+				if [ $ret -eq 0 ]; then
+					echo -e "$BOLDGREEN[+] Preparing Modules Succeed$ENDCOLOR"
+				else
+					echo -e "$BOLDRED[-] Preparing Modules Failed (exit code: $ret)$ENDCOLOR"
+				fi
+			fi
+			;;
+		4)
+			echo -e "$BOLDGREEN[+] Coping Modules to out_modules$ENDCOLOR"
+			if [ ! -f "$KDIR/$OUT_DIR/.config" ]; then
+				echo -e "$BOLDRED[-] No .config found$ENDCOLOR"
+			else
+				echo -e "$BOLDGREEN[+] .config found$ENDCOLOR"
+				make -C "$KDIR" O="$OUT_DIR" $KERNEL_ENV INSTALL_MOD_PATH="../out_modules" CONFIG_SECTION_MISMATCH_WARN_ONLY=y modules_install -j"$(nproc)"
+				ret=$?
+				if [ $ret -eq 0 ]; then
+					echo -e "$BOLDGREEN[+] Coping Building Succeed$ENDCOLOR"
+					echo -e "$BOLDGREEN[+] You can find the modules in out_modules$ENDCOLOR"
+				else
+					echo -e "$BOLDRED[-] Coping Building Failed (exit code: $ret)$ENDCOLOR"
+				fi
+			fi
+			;;
+		5)
+			echo -e "$BOLDGREEN[+] Cleaning$ENDCOLOR"
+			rm -rf out_modules
+			make -s -C "$KDIR" O="$OUT_DIR" $KERNEL_ENV CONFIG_SECTION_MISMATCH_WARN_ONLY=y clean -j"$(nproc)" && make -s -C "$KDIR" O="$OUT_DIR" $KERNEL_ENV CONFIG_SECTION_MISMATCH_WARN_ONLY=y mrproper -j"$(nproc)"
+			ret=$?
+			if [ $ret -eq 0 ]; then
+				echo -e "$BOLDGREEN[+] Cleaning Succeed$ENDCOLOR"
+			else
+				echo -e "$BOLDRED[-] Cleaning Failed (exit code: $ret)$ENDCOLOR"
+			fi
+			;;
+		6)
+			echo -ne "$BOLDGREEN[+] Enter the defconfig's name: $ENDCOLOR"
+			read config
+			if [ ! -f "$KDIR/arch/$ARCH/configs/$config" ]; then
+				echo -e "$BOLDRED[-] $config is not found$ENDCOLOR"
+			else
+				make -C "$KDIR" O="$OUT_DIR" $KERNEL_ENV CONFIG_SECTION_MISMATCH_WARN_ONLY=y $config -j"$(nproc)"
+				ret=$?
+				if [ $ret -eq 0 ]; then
+					echo -e "$BOLDGREEN[+] Config Applying Succeed$ENDCOLOR"
+				else
+					echo -e "$BOLDRED[-] Config Applying Failed (exit code: $ret)$ENDCOLOR"
+				fi
+			fi
+			;;
+		7)
+			echo -e "$BOLDGREEN[+] Editing Config (MENUCONFIG)$ENDCOLOR"
+			make -C "$KDIR" O="$OUT_DIR" $KERNEL_ENV CONFIG_SECTION_MISMATCH_WARN_ONLY=y menuconfig -j"$(nproc)"
+			;;
+		8)
+			echo -e "$BOLDGREEN[+] Editing Config (NCONFIG)$ENDCOLOR"
+			make -C "$KDIR" O="$OUT_DIR" $KERNEL_ENV CONFIG_SECTION_MISMATCH_WARN_ONLY=y nconfig -j"$(nproc)"
+			;;
+		9)
+			echo -e "$BOLDGREEN[+] Saving current .config as new defconfig$ENDCOLOR"
+			if [ ! -f "$KDIR/$OUT_DIR/.config" ]; then
+				echo -e "$BOLDRED[!] .config is not found$ENDCOLOR"
+			else
+				echo -ne "$BOLDGREEN[!] Enter the new defconfig name: $ENDCOLOR"
+				read newconfig
+				if [ -f "$KDIR/arch/$ARCH/configs/$newconfig" ]; then
+					echo -e "$BOLDRED[!] $newconfig is found"
+					echo -ne "Do you want to overwrite it?$ENDCOLOR [y,N]: "
+					read answer
+					case "$answer" in
+						Y|y)
+							echo -e "$BOLDGREEN[+] Overwriting current .config as $newconfig$ENDCOLOR"
+							cp "$KDIR/$OUT_DIR/.config" "$KDIR/arch/$ARCH/configs/$newconfig"
+							ret=$?
+							if [ $ret -eq 0 ]; then
+								echo -e "$BOLDGREEN[+] Coping succeed$ENDCOLOR"
+							else
+								echo -e "$BOLDRED[-] Coping Failed (exit code: $ret)$ENDCOLOR"
+							fi
+							;;
+						N|n|'')
+							echo -e "$BOLDGREEN[!] Skipped Saving the new defconfig$ENDCOLOR"
+							;;
+					esac
+				else
+					echo -e "$BOLDGREEN[+] Saving current .config as $newconfig$ENDCOLOR"
+					cp "$KDIR/$OUT_DIR/.config" "$KDIR/arch/$ARCH/configs/$newconfig"
+					ret=$?
+					if [ $ret -eq 0 ]; then
+						echo -e "$BOLDGREEN[+] Coping succeed$ENDCOLOR"
+					else
+						echo -e "$BOLDRED[-] Coping Failed (exit code: $ret)$ENDCOLOR"
+					fi
+				fi
+			fi
+			;;
+		E)
+			echo -e "$BOLDRED[!] Exiting!!$ENDCOLOR"
+			break
+			;;
+		G)
+			echo -e "$BOLDGREEN[+] Opening the creator's github page$ENDCOLOR"
+			xdg-open https://github.com/karamdev1 > /dev/null 2>&1 &
+			;;
+		*)
+			echo -e "$BOLDRED[!] Invalid Action!!$ENDCOLOR"
+			;;
+	esac
+	echo -ne "${BOLDYELLOW}Press enter to continue$ENDCOLOR"
+	read
+done
