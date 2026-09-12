75f8fbdd707178300b794ed4156e61a0a775a5a8
karamdev1 <karamgaggi100@gmail.com>
2026-09-12 10:52:15 +0300

    Patching scripts/gcc-wrapper.py to parse compiler warnings without throwing python errors

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -28,7 +28,7 @@ ofile = None
 warning_re = re.compile(r'''(.*/|)([^/]+\.[a-z]+:\d+):(\d+:)? warning:''')
 def interpret_warning(line):
     """Decode the message from gcc.  The messages we care about have a filename, and a warning"""
-    line = line.rstrip('\n')
+    line = line.decode('utf-8', errors='ignore').rstrip('\n')
     m = warning_re.match(line)
     if m and m.group(2) not in allowed_warnings:
         print("error, forbidden warning:", m.group(2))
